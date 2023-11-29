a3d138c9adb884691855ff5a3aa2cedf812afda6
Andre A. Mesquita <andre20mesquita@gmail.com>
2023-11-29 03:05:19 -0300

    subindo as rotas e modificações na controller do usuario para login e cadastro

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return "ok";
+        try {
+            $user = User::where([
+                'email' => $request->email
+            ])->first();
+
+            if (Auth::attempt(['email' => trim($request->email), 'password' => trim($request->password)])) {
+                return response("usuário logado", 200);
+            } else {
+                return response("email/password inválidos", 200);
+            }
+        } catch (\Exception $e) {
+            return response($e->getMessage(), 400);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            User::create([
+                'name' => 'default',
+                'email' => $request->email,
+                'password' => bcrypt($request->password)
+            ]);
+        } catch (\Exception $e) {
+            return response($e->getMessage(), 400);
+        }
+
+        return response("Usuario cadastrado com sucesso", 200);
     }
 }
